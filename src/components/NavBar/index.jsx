b8000f7d3299826270bb2733b5010e3ce9deeee0
shawn <shawnantonucci2018@gmail.com>
2019-08-29 21:56:12 -0700

    added burger icon to mobile navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { Navbar } from "react-bulma-components";
 
 export default class NavbarComp extends Component {
+    state = {
+        open: false
+    };
     static propTypes = {
         userSession: PropTypes.object.isRequired
     };
@@ -14,28 +17,31 @@ export default class NavbarComp extends Component {
         window.location = "/";
     };
 
+    toggleNavbar = () => {
+        this.setState({ open: !this.state.open });
+    };
+
     render() {
         const { userSession } = this.props;
+        const { open } = this.state;
         const isSignedIn = userSession.isUserSignedIn();
 
         return (
-            <Navbar color="info" fixed="top">
+            <Navbar color="info" fixed="top" active={open}>
                 <Navbar.Brand>
                     <Navbar.Item>
                         <p>Blog Stacks</p>
                     </Navbar.Item>
+
+                    <Navbar.Burger onClick={this.toggleNavbar} />
                 </Navbar.Brand>
 
                 <Navbar.Menu>
                     <Navbar.Container position="end">
                         {isSignedIn && (
                             <React.Fragment>
-                                <Navbar.Item>
-                                    Posts
-                                </Navbar.Item>
-                                <Navbar.Item>
-                                    My Profile
-                                </Navbar.Item>
+                                <Navbar.Item>Posts</Navbar.Item>
+                                <Navbar.Item>My Profile</Navbar.Item>
                                 <Navbar.Item onClick={this.handleSignOut}>
                                     SignOut
                                 </Navbar.Item>
