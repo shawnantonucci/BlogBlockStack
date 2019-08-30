35334288313605c6914c79b12ac977085ce32f27
shawn <shawnantonucci2018@gmail.com>
2019-08-29 18:51:17 -0700

    setup repo & added signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import "./stylesheets/main.scss"
-import { Button } from 'react-bulma-components';
+import React, { Component } from "react";
+import "stylesheets/main.scss";
+import { Button } from "react-bulma-components";
+import { appConfig } from "utils/contants";
+import { UserSession } from "blockstack";
 
-function App() {
-  return (
-    <div className="App">
-      <Button color="primary">
-        Bulma Button
-      </Button>
-    </div>
-  );
+class App extends Component {
+    state = {
+        userSession: new UserSession({ appConfig })
+    };
+
+    handleSignIn = () => {
+        const { userSession } = this.state;
+        userSession.redirectToSignIn();
+    };
+
+    handleSignOut = () => {
+        console.log("Handle Sign Out");
+    };
+
+    render() {
+        const { userSession } = this.state;
+
+        return (
+            <div className="App">
+                {userSession.isUserSignedIn() ? (
+                    <Button color="primary" onClick={this.handleSignOut}>
+                        Sign Out
+                    </Button>
+                ) : (
+                    <Button color="primary" onClick={this.handleSignIn}>
+                        Sign In
+                    </Button>
+                )}
+            </div>
+        );
+    }
 }
 
 export default App;
